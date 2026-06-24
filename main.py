5466121901eabeb2e68b9d4f4de64d04180bfff1
Denisse <arxie.denisse@gmail.com>
2026-06-24 11:49:40 -0700

    FirstCommit

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+# App Init
+app = FastAPI(
+    title="API System",
+    description="API service to print Hello World",
+    version="1.0.0"
+)
+
+# Root endpoint
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
